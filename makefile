21a4fde7db9b6fc6a2c48e4fc7e1c5458c4e5481
fiddlesticks971 <mitchaheim@gmail.com>
2017-05-30 20:27:51 -0500

    making progress

--- makefile
+++ makefile
@@ -1,14 +1,13 @@
-C_FLAGS = -std=c++11
+C_FLAGS = -std=c++14
 L_FLAGS = -g -lglfw -lvulkan
 
 main:  vulkanTest.cpp
-	echo "Making Hello World File"
 	g++ $(C_FLAGS) vulkanTest.cpp -o vulkanTest $(L_FLAGS)
 
-triangle: triangle.cpp
-	echo "Making the Triangle"
+triangle: triangle.cpp 
 	g++ $(C_FLAGS) triangle.cpp -o triangle $(L_FLAGS)
 
 clean:
 	rm vulkanTest
-	echo "Now Clean"
+	rm triangle
+	rm VDeleter
--- triangle.cpp
+++ triangle.cpp
@@ -2,12 +2,20 @@
 #include <iostream>
 #include <stdexcept>
 #include <functional>
+#include <vector>
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
 const int HEIGHT = 600;
 const int WIDTH = 800;
+
+struct QueueFamilyIndices {
+  int graphicsFamily = -1;
+  bool isComplete() {
+    return graphicsFamily >= 0;
+  }
+};
 
 class HelloTriangleApplication {
 public:
@@ -21,26 +29,159 @@ private:
   void initWindow(){
     if(!glfwInit())
       std::cout<<"glfw Init failed\n";
+    
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
     window = glfwCreateWindow(WIDTH,HEIGHT,"Triangle",nullptr,nullptr);
     if(!window)
       std::cout<<"window failed\n";
-    
   }
   void initVulkan() {
+    createInstance();
+    pickPhysicalDevice();
+    createLogicalDevice();
+    
   }
   void mainLoop() {
     while(!glfwWindowShouldClose(window)) {
       glfwPollEvents();
+      //std::cout<<"itworked\n";
     }
+    
+  }
+  void cleanUp(){
+    vkDestroyDevice(device, nullptr);
+    vkDestroyInstance(instance, nullptr);
     glfwDestroyWindow(window);
     glfwTerminate();
   }
-  void cleanUp(){
+
+  void createInstance(){
+    VkApplicationInfo appInfo = {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    unsigned int glfwExtensionCount = 0;
+    const char** glfwExtensions;
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+    createInfo.enabledLayerCount = 0;
+    VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+    if( result != VK_SUCCESS)
+      std::cout<<"VK failed to initialize\n";
+  }
+
+  void pickPhysicalDevice(){
+    physicalDevice = VK_NULL_HANDLE;
+    
+    uint32_t deviceCount = 0;
+    
+    vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+    
+    if (deviceCount == 0) {
+      throw std::runtime_error("failed to find GPUs with Vulkan support!");
+    }
+    
+    std::vector<VkPhysicalDevice> devices(deviceCount);
+    vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+    
+    for (const auto& singleDevice : devices) {
+      //std::cout<<device<<std::endl;
+      if (isDeviceSuitable(singleDevice)) {
+        physicalDevice = singleDevice;
+        break;
+      }
+    }
+    //RADV is giving errors but it should comment this line out if it causes problems
+    /*************************************/
+    physicalDevice = devices[0];
+    /************************************/
+
+    if (physicalDevice == VK_NULL_HANDLE) {
+      throw std::runtime_error("failed to find a suitable GPU!");
+    }
+  }
+
+  bool isDeviceSuitable(VkPhysicalDevice device) {
+    QueueFamilyIndices indices = findQueueFamilies(device);
+
+    return indices.isComplete();
+  }
+
+  QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
+    QueueFamilyIndices indices;
+    
+    uint32_t queueFamilyCount = 0;
+
+    std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+    int i = 0;
+    for (const auto& queueFamily : queueFamilies) {
+      if (queueFamily.queueCount > 0 && queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+        indices.graphicsFamily = i;
+      }
+
+      if (indices.isComplete()) {
+        break;
+      }
+
+      i++;
+    }
+
+    return indices;
+  }
+
+  void createLogicalDevice() {
+    QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+    
+    VkDeviceQueueCreateInfo queueCreateInfo = {};
+    queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    queueCreateInfo.queueFamilyIndex = indices.graphicsFamily;
+    queueCreateInfo.queueCount = 1;
+
+    float queuePriority = 1.0f;
+    queueCreateInfo.pQueuePriorities = &queuePriority;
+
+    VkPhysicalDeviceFeatures deviceFeatures = {};
+
+    VkDeviceCreateInfo createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+
+    createInfo.pQueueCreateInfos = &queueCreateInfo;
+    createInfo.queueCreateInfoCount = 1;
+
+    createInfo.pEnabledFeatures = &deviceFeatures;
+
+    createInfo.enabledExtensionCount = 0;
+
+    //uncomment for using validation layers
+    /*if (enableValidationLayers) {
+      createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+      createInfo.ppEnabledLayerNames = validationLayers.data();
+    } else {*/
+    createInfo.enabledLayerCount = 0;
+    /*}*/
+
+    if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
+      throw std::runtime_error("failed to create logical device!");
+    }
+    
   }
   
   GLFWwindow* window;
+  VkInstance instance;
+  VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+  VkDevice device;
   
 };
 
